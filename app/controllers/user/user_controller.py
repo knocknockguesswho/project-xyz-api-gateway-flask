11173dcd28564b7ac1ed8579488ac2e4e1681e88
faris <nullengineer@gmail.com>
2023-01-14 23:02:22 +0700

    Chore: remove unused controller

--- app/controllers/user/user_controller.py
+++ app/controllers/user/user_controller.py
@@ -5,19 +5,5 @@ from app.helpers.response_helper import ResponseHelper
 from app.helpers.jwt_helper import JWTHelper
 
 class UserController:
-  def retrieve_profile(self):
-    response_helper = ResponseHelper()
-    jwt_helper = JWTHelper()
-    try:
-      # get user id from decoded access token
-      access_token = request.headers.get('Authorization')
-      user_id = jwt_helper.decode_token(token=access_token)['user_id']
-      res = requests.get(f'http://127.0.0.1:5001/get-by-id/{user_id}')
-      res = json.loads(res.text)
-      response_helper.set_data(res['data'])
-    except Exception as e:
-      msg = str(e)
-      status = 400
-      response_helper.set_to_failed(msg, status)
-    finally:
-      return response_helper.get_response()
+  def find_friend(self, **params):
+    return
