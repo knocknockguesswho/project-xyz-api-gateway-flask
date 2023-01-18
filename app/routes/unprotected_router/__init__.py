25c4ef4890e811c5ff87903ec8f58db3bdf0cec1
faris <nullengineer@gmail.com>
2023-01-18 22:05:20 +0700

    Update(unprotected router): add new endpoint

--- app/routes/unprotected_router/__init__.py
+++ app/routes/unprotected_router/__init__.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 from app.controllers.auth.auth_controller import AuthController
+from app.controllers.user.user_controller import UserController
 from app.middlewares.session_middleware import session_middleware
 
 blueprint = Blueprint('unprotected_router', __name__)
@@ -13,3 +14,6 @@ def register(): return AuthController().register()
 @blueprint.route('/auth/refresh-token', methods=['POST'])
 @session_middleware
 def refresh_token(): return AuthController().refresh_token()
+
+@blueprint.route('/find-user-by-username/<string:username>')
+def find_user_by_username(username: str): return UserController().find_user_by_username(username=username)
