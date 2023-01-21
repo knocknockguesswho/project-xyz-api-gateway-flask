15fc685380a8868f3ff21182e0f0d40b7cc77168
faris <nullengineer@gmail.com>
2023-01-21 16:51:52 +0700

    Update(schema helper): add new schema of signup form

---
+++ app/helpers/schema_helper.py
@@ -0,0 +1,9 @@
+class SignupFormSchema:
+  def __init__(self):
+    self.first_name: str = str()
+    self.last_name: str = str()
+    self.password: str = str()
+    self.username: str = str()
+    self.email: str = str()
+    self.birth_date: str = str()
+    self.country_code: str = str()
