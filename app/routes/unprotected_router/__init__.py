60b36f4d4fddfdaf03667c6eda60b79cb53e4325
faris <nullengineer@gmail.com>
2023-01-22 19:05:34 +0700

    Update(unprotected router): add service name prefix on endpoint value

--- app/routes/unprotected_router/__init__.py
+++ app/routes/unprotected_router/__init__.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 from app.controllers.auth.auth_controller import AuthController
 from app.controllers.user.user_controller import UserController
+from app.controllers.external.country_code_controller import CountryCodeController
 from app.middlewares.session_middleware import session_middleware
 from app.middlewares.signup_middleware import signup_middleware
 
@@ -17,5 +18,5 @@ def signup(): return UserController().signup()
 @session_middleware
 def refresh_token(): return AuthController().refresh_token()
 
-@blueprint.route('/find-user-by-username/<string:username>')
+@blueprint.route('/user/find-user-by-username/<string:username>')
 def find_user_by_username(username: str): return UserController().find_user_by_username(username=username)
